239b591c407a3cb959a9730659e2d5675d485f0e
chenc <git@zzfzzf.com>
2021-06-24 00:02:01 +0800

    feat: 添加性能

--- src/Os.ts
+++ src/Os.ts
@@ -2,17 +2,33 @@
  * 获取用户设备信息
  */
 export class Os {
+    private readonly performanceTiming: PerformanceTiming
+
+    constructor() {
+        this.performanceTiming = window.performance.timing
+    }
+
     /**
      * 用户操作系统
      */
-    static getPlatform(){
+    static getPlatform() {
         return window?.navigator?.platform
     }
 
     /**
      * 用户浏览器
      */
-    static getBrowser(){
+    static getBrowser() {
         return window.navigator.userAgent
+    }
+
+    getFpt() {
+        const {domLoading, navigationStart} = this.performanceTiming
+        return domLoading - navigationStart
+    }
+
+    getTti() {
+        const {domInteractive, navigationStart} = this.performanceTiming
+        return domInteractive - navigationStart
     }
 }
